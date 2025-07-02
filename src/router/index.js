86df830cea921bceb395d4f597b17d57f5ff375e
jongdu <parksn5029@naver.com>
2025-07-02 09:24:46 +0900

    [feat] router-index 누락 파일 업로드

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import DashboardView from '@/views/dashboard/DashboardView.vue';
+import ProductsView from '@/views/products/ProductsView.vue';
+import ProductsDetailView from '@/views/products/ProductsDetailView.vue';
+import ProductsCreateView from '@/views/products/ProductsCreateView.vue';
+import InventoryView from '@/views/inventory/InventoryView.vue';
+import ProductsUpdateView from '@/views/products/ProductsUpdateView.vue';
+import InventoryCreateView from '@/views/inventory/InventoryCreateView.vue';
+import InventoryDetailView from '@/views/inventory/InventoryDetailView.vue';
+import InventoryUpdateView from '@/views/inventory/InventoryUpdateView.vue';
 
 const routes = [
 	/**
@@ -11,22 +19,77 @@ const routes = [
 		name: 'Dashboard',
 		component: DashboardView,
 	},
+	{
+		path: '/products',
+		name: 'Products',
+		component: ProductsView,
+	},
 
 	/**
 	 * 재고
 	 */
 
-	// 재고 현황 상세 페이지
+	//재고 리스트 페이지
+	{
+		path: '/inventories',
+		name: 'Inventories',
+		component: InventoryView,
+	},
+
+	//재고 신규 등록 페이지
+	{
+		path: '/inventories/new',
+		name: 'InventoryCreate',
+		component: InventoryCreateView,
+	},
+
+	//재고 현황 상세 페이지
 	{
 		path: '/inventories/:id',
 		name: 'InventoryDetail',
-		component: null,
+		component: InventoryDetailView,
+		props: true,
+	},
+
+	//재고 수정 페이지
+	{
+		path: '/inventories/:id/edit',
+		name: 'InventoryUpdate',
+		component: InventoryUpdateView,
+		props: true,
+	},
+
+	/**
+	 * 상품
+	 */
+
+	//상품 신규 등록
+	{
+		path: '/products/new',
+		name: 'ProductsCreate',
+		component: ProductsCreateView,
+	},
+
+	//상품 상세 페이지
+	{
+		path: '/products/:id',
+		name: 'ProductsDetail',
+		component: ProductsDetailView,
+		props: true,
+	},
+
+	//상품 수정
+	{
+		path: '/products/:id/edit',
+		name: 'ProductsUpdate',
+		component: ProductsUpdateView,
 		props: true,
 	},
 
 	/**
 	 * 주문
 	 */
+
 	// 주문 현황 상세 페이지
 	{
 		path: '/orders/:id',
